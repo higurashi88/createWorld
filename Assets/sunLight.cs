73e7e1eff684c2717fe60fd14a66d0c07364b996
higurashi88 <msippc1997@gmail.com>
2018-12-31 02:23:50 +0900

    時間追加

---
+++ Assets/sunLight.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class sunLight : MonoBehaviour {
+    [SerializeField]
+    private float sunSpeed=0.1f;
+    [SerializeField]
+    private Vector3 rotation = new Vector3(270f, 330f, 0f);
+	// Use this for initialization
+	void Start () {
+        transform.localRotation = Quaternion.Euler(rotation);
+
+        var rotX = transform.localEulerAngles.x - 15f * DateTime.Now.Hour;
+        if(rotX < 0)
+        {
+            rotX += 360;
+        }
+        transform.localEulerAngles = new Vector3(rotX, transform.localEulerAngles.y, transform.localEulerAngles.z);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(-Vector3.right * sunSpeed * Time.deltaTime);
+	}
+}
---
+++ Assets/worldSettings.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class worldSettings : MonoBehaviour {
+    public int WorldMagic=0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        WorldMagic += 10;
+	}
+}
