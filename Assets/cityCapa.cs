3ea5deafd7e19ec6fb70bd4be18acd5bd7d00c4e
higurashi88 <msippc1997@gmail.com>
2018-12-31 03:21:53 +0900

    地形変更、街の発展度、行商人(未完成)追加

---
+++ Assets/cityCapa.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cityCapa : MonoBehaviour {
+    public GameObject hawker;
+    public GameObject target;
+
+    public float develop = 100f;//町の発展度
+    public int popu;//町の人口
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (0 < popu)
+        {
+            Instantiate(hawker);
+            popu--;
+        } 
+	}
+}
