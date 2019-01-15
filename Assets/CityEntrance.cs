2965b586ad9ebc51025d51cea271e2ec905c1d68
higurashi88 <msippc1997@gmail.com>
2019-01-16 01:42:29 +0900

    hawkers motion

---
+++ Assets/CityEntrance.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CityEntrance : MonoBehaviour {
+    public GameObject hawker;//行商人オブジェクトを定義
+    private GameObject obj;
+    public GameObject entrance;
+
+    public static GameObject target;
+    private int num = 5;//行商人の数
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (num >= 0)
+        {
+            obj=Instantiate(hawker, this.transform.position, Quaternion.identity);//行商人を生成
+            obj.transform.parent = entrance.transform;
+            num--;
+        }
+    }
+}
--- Assets/hawkers.cs
+++ Assets/hawkers.cs
@@ -6,15 +6,15 @@ public class hawkers : MonoBehaviour {
 
     public Transform target;//移動先の街
     public float speed = 0.1f;//移動スピード
-
+    private GameObject parent;
 	// Use this for initialization
 	void Start () {
-        target = cityCapa.target;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(target.position - transform.position), 0.3f);
+        parent= this.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(parent.transform.position - transform.position), 0.3f);
         transform.position += transform.forward * speed;
     }
 }
