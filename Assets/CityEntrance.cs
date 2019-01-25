cdd472b072a6a6c49d511c4c1011208cf4b078e9
higurashi88 <msippc1997@gmail.com>
2019-01-26 01:33:13 +0900

    hawersMotion

--- Assets/CityEntrance.cs
+++ Assets/CityEntrance.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 
 public class CityEntrance : MonoBehaviour {
     public GameObject hawker;//行商人オブジェクトを定義
-    private GameObject obj;
+    //private GameObject obj;
     public GameObject entrance;
+    //public float timeout=60f;
+    //private float timeElapsed;
 
     public static GameObject target;
     private int num = 5;//行商人の数
@@ -18,11 +20,30 @@ public class CityEntrance : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (num >= 0)
+        // timeElapsed += Time.deltaTime;
+        //if (timeElapsed >= timeout)
+        //{
+        num = popuManage(num);
+           // timeElapsed = 0.0f;
+        //}
+    }
+    public int popuManage (int x)
+    {
+        if (x <= 0) return 0;
+        GameObject obj = Instantiate(hawker, new Vector3(this.transform.position.x,this.transform.position.y+5,this.transform.position.z), Quaternion.identity);//行商人を生成
+        obj.transform.parent = entrance.transform;
+        obj.transform.localScale = Vector3.one;
+        x--;
+        return x;
+    }
+
+    private void OnCollisionStay(Collision other)
+    {
+        if (other.gameObject.tag == "NPC")
         {
-            obj=Instantiate(hawker, this.transform.position, Quaternion.identity);//行商人を生成
-            obj.transform.parent = entrance.transform;
-            num--;
+            Destroy(other.gameObject);
+            num++;
         }
     }
+
 }
